33acf70e189fa73dc7a9139c00079284564fe900
simoni <166508581+SimoniMoradiya@users.noreply.github.com>
2024-04-27 09:35:50 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CurrencyConverter from './CurrencyConverter';
+
+function App() {
+  return (
+    <div className="App">
+      <CurrencyConverter />
+    </div>
+  );
+}
+
+export default App;
